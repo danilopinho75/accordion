33fe15f9228051b376c995dd0e6b8ab4b1e25db5
danilopinho75 <danilopinho11@gmail.com>
2024-07-02 15:03:13 -0300

    Acordeão realizado com html, css e javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const accordion = document.querySelectorAll('.js-accordion')
+
+accordion.forEach(acc => {
+    acc.addEventListener('click', () => {
+        let pai = acc.parentElement;
+        pai.classList.toggle('active');
+    })
+})
